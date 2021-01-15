39ac620b60de375fb6aac7cd85879f20fb6ae64a
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-15 11:20:37 -0500

    Add project files.

---
+++ Tutorial1-StructsMethodsArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * @author  Nick Sturch-Flint
+ * @version 1.0.0
+ * @since   2021-01-15
+ * @see     OOP4200 Tutorial 1
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1_StructsMethodsArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
