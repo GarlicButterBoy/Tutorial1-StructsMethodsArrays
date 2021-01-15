c2f704df83f943c34f31407b4d66688f1f51aa58
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-15 11:30:34 -0500

    Added PercentToLetterGrade Method

--- Tutorial1-StructsMethodsArrays/Program.cs
+++ Tutorial1-StructsMethodsArrays/Program.cs
@@ -17,6 +17,48 @@ namespace Tutorial1_StructsMethodsArrays
     {
         static void Main(string[] args)
         {
+
+
+
         }
+        /// <summary>
+        /// PercentToLetterGrade - Converts numeric grade to letter grade
+        /// </summary>
+        /// <param name="percentageGrade"></param>
+        /// <returns></returns>
+        public static string PercentToLetterGrade(double percentageGrade)
+        {
+            const double MIN_GRADE = 0.0;   //Min Grade Allowed
+            const double MAX_GRADE = 100.0; //Max Grade Allowed
+            string strFeedback = "INVALID"; //Invalid by default
+
+            //If numeric grade is a valid number
+            if (percentageGrade >= MIN_GRADE && percentageGrade <= MAX_GRADE)
+            {
+                //Sets the letter grade depending on numeric input
+                if (percentageGrade >= 90.0)
+                    strFeedback = "A+";
+                else if (percentageGrade >= 85.0)
+                    strFeedback = "A";
+                else if (percentageGrade >= 80.0)
+                    strFeedback = "A-";
+                else if (percentageGrade >= 75.0)
+                    strFeedback = "B+";
+                else if (percentageGrade >= 70.0)
+                    strFeedback = "B";
+                else if (percentageGrade >= 65.0)
+                    strFeedback = "C+";
+                else if (percentageGrade >= 60.0)
+                    strFeedback = "C";
+                else if (percentageGrade >= 55.0)
+                    strFeedback = "D+";
+                else if (percentageGrade >= 50.0)
+                    strFeedback = "D";
+                else
+                    strFeedback = "F";
+            }
+            return strFeedback; //returns strFeedback
+        }
+
     }
 }
