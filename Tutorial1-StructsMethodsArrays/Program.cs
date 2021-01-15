86c2e98faad899251c139509f3ea619b32a52d5f
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-15 11:54:16 -0500

    Added Struct, TryCatch Block, and Delegate

--- Tutorial1-StructsMethodsArrays/Program.cs
+++ Tutorial1-StructsMethodsArrays/Program.cs
@@ -13,19 +13,50 @@ using System.Threading.Tasks;
 
 namespace Tutorial1_StructsMethodsArrays
 {
+    /// <summary>
+    /// A data structure to store an average grade, number of passing grades, number of failures and number of invalid grades
+    /// </summary>
+    struct GradeStats
+    {
+        public double averageGrade; //Average grade for all valid grades
+        public int passCount;       //Number of passing grades
+        public int failCount;       //Number of failing grades
+        public int invalidCount;    //Number of invalid grades
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //Delegate Declaration, initialized with PercentToLetterGrade Method
+            PercentToFeedback feedbackMethod = PercentToLetterGrade;
 
+            //Change PercentToFeedback from one Method (PercentToLetterGrade) to another (PercentToDescription)
+            try
+            {
+                if (args[0].ToLower() == "description")
+                    feedbackMethod = PercentToDescription;
+            }
+            catch (Exception) { ;}
 
-
+            //End of Program
+            Console.WriteLine("\nPress Any Key to Exit...");
+            Console.ReadKey();
         }
+
+        /// <summary>
+        /// Used to reference either PercentToGrade() or PercentToDescription
+        /// </summary>
+        /// <param name="percentageGrade">Numeric Grade</param>
+        /// <returns>Feedback String</returns>
+        public delegate string PercentToFeedback(double percentageGrade);
+
+
         /// <summary>
         /// PercentToLetterGrade - Converts numeric grade to letter grade
         /// </summary>
-        /// <param name="percentageGrade"></param>
-        /// <returns></returns>
+        /// <param name="percentageGrade">Numeric Grade</param>
+        /// <returns>Feedback String</returns>
         public static string PercentToLetterGrade(double percentageGrade)
         {
             const double MIN_GRADE = 0.0;   //Min Grade Allowed
@@ -63,8 +94,8 @@ namespace Tutorial1_StructsMethodsArrays
         /// <summary>
         /// Converts a numeric grade to a descriptive string
         /// </summary>
-        /// <param name="percentageGrade"></param>
-        /// <returns></returns>
+        /// <param name="percentageGrade">Numeric Grade</param>
+        /// <returns>Feedback String</returns>
         public static string PercentToDescription(double percentageGrade)
         {
             const double MIN_GRADE = 0.0;   //Min Grade Allowed
@@ -89,8 +120,6 @@ namespace Tutorial1_StructsMethodsArrays
                     strFeedback = "Satisfactory";
                 else if (percentageGrade >= 60.0)
                     strFeedback = "Acceptable";
-                else if (percentageGrade >= 55.0)
-                    strFeedback = "Conditional Pass";
                 else if (percentageGrade >= 50.0)
                     strFeedback = "Conditional Pass";
                 else
