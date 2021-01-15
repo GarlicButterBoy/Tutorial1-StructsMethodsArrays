56d362ef5792541ae7a79d135efdd5ef8e918730
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-15 11:34:49 -0500

    Added PercentToDescription Method

--- Tutorial1-StructsMethodsArrays/Program.cs
+++ Tutorial1-StructsMethodsArrays/Program.cs
@@ -60,5 +60,44 @@ namespace Tutorial1_StructsMethodsArrays
             return strFeedback; //returns strFeedback
         }
 
+        /// <summary>
+        /// Converts a numeric grade to a descriptive string
+        /// </summary>
+        /// <param name="percentageGrade"></param>
+        /// <returns></returns>
+        public static string PercentToDescription(double percentageGrade)
+        {
+            const double MIN_GRADE = 0.0;   //Min Grade Allowed
+            const double MAX_GRADE = 100.0; //Max Grade Allowed
+            string strFeedback = "INVALID"; //Invalid by default
+            
+            //If numeric grade is a valid number
+            if (percentageGrade >= MIN_GRADE && percentageGrade <= MAX_GRADE)
+            {
+                //Sets the letter grade depending on numeric input
+                if (percentageGrade >= 90.0)
+                    strFeedback = "Outstanding";
+                else if (percentageGrade >= 85.0)
+                    strFeedback = "Exemplary";
+                else if (percentageGrade >= 80.0)
+                    strFeedback = "Excellent";
+                else if (percentageGrade >= 75.0)
+                    strFeedback = "Very Good";
+                else if (percentageGrade >= 70.0)
+                    strFeedback = "Good";
+                else if (percentageGrade >= 65.0)
+                    strFeedback = "Satisfactory";
+                else if (percentageGrade >= 60.0)
+                    strFeedback = "Acceptable";
+                else if (percentageGrade >= 55.0)
+                    strFeedback = "Conditional Pass";
+                else if (percentageGrade >= 50.0)
+                    strFeedback = "Conditional Pass";
+                else
+                    strFeedback = "Failure";
+            }
+            return strFeedback; //returns strFeedback
+        }
+
     }
 }
